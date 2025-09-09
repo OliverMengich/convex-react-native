3327f2533771a6171719af3bbe5229dcf521eb01
Oliver Kipkemei <55626791+OliverMengich@users.noreply.github.com>
2025-09-09 09:45:20 +0300

    chore: convex auth setup

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,24 +1,42 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
+import {ConvexReactClient} from 'convex/react';
+import { ConvexAuthProvider } from '@convex-dev/auth/react'
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
-
+import * as SecureStore from "expo-secure-store";
 import { useColorScheme } from '@/hooks/use-color-scheme';
+import { Platform } from 'react-native';
+import * as SplashScreen from 'expo-splash-screen';
 
+import Paths from '@/components/paths';
 export const unstable_settings = {
-  anchor: '(tabs)',
+  	anchor: '(tabs)',
 };
 
-export default function RootLayout() {
-  const colorScheme = useColorScheme();
+const convex = new ConvexReactClient(process.env.EXPO_PUBLIC_CONVEX_URL!, {
+  	unsavedChangesWarning: false,
+});
+const secureStorage = {
+	getItem: SecureStore.getItemAsync,
+	setItem: SecureStore.setItemAsync,
+	removeItem: SecureStore.deleteItemAsync,
+};
+SplashScreen.preventAutoHideAsync();
+export default function RootLayout() { 
+	const colorScheme = useColorScheme();
 
-  return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
-  );
+	return (
+		<ConvexAuthProvider client={convex} 
+			storage={
+					Platform.OS === "android" || Platform.OS === "ios"
+					? secureStorage
+					: undefined
+				}
+			>
+			<ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+				<Paths/>
+				<StatusBar style="auto" />
+			</ThemeProvider>
+		</ConvexAuthProvider>
+	);
 }
